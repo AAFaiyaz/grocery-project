c526da70fe3f3114cd5d64ecaf7a19ac6325b34a
Ashikur Rahman <ashikurjhalak@gmail.com>
2020-04-22 21:03:39 +0600

    indextin dynamic product

--- indextin.php
+++ indextin.php
@@ -1,8 +1,25 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $query ="SELECT * FROM `products` ORDER BY product_id ASC";
+
+    try {
+        $connect = new PDO("mysql:host=$servername;dbname=poti", $username, $password);
+
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $connect->query($query);
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>  
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title></title>
+    <title>Grocery Product</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="css/fontawesome.min.css" rel="stylesheet">
@@ -147,6 +164,50 @@
     <div class="top-right-frame">
         <div class="header">
             <h3>Products</h3>
+            
+            <div class="row container">
+
+                    
+            <?php
+                    foreach($result as $row)
+                    {
+                    echo '
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">                                              
+                        
+                            <a href="#"><img class="card-img-top" src="img\TubIceCream.jpg" alt=""></a>
+                                        
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="#">'.$row["product_name"].'</a>
+                                </h4>
+                                <h5>$'.$row["unit_price"].'</h5>
+                                <p class="card-text">Quantity : '.$row["unit_quantity"].' </p>
+                                <p class="card-text">In-Stock : '.$row["in_stock"].'</p>
+                            </div>
+                            
+                            
+                            
+                            <div class="card-footer">
+                                <small class="pull-left">
+                                    <a href="#" id="btn-standard" class="btn btn-outline-success">
+                                        Add <i class="fas fa-cart-plus"></i> 
+                                    </a>
+                                </small>
+                                <small class="pull-right">
+                                    <a href="#" class="btn btn-warning">
+                                        Cart <i class="fas fa-shopping-bag"></i> 
+                                    </a>
+                            </small>
+                            </div>
+                        </div>
+                    </div>
+                    ';
+                    break;
+                    }
+                    ?>
+                
+            </div>
         </div>
     </div>
     <div class="bottom-right-frame">
@@ -155,49 +216,39 @@
         </div>
     </div>
 
-    <!-- </div> -->
+    <!-- Back-To-Top -->
+    <a href="#" id="back-to-top" style="display: none;"><span></span></a>
 
-
+    
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <script src="js/fontawesome.min.js"></script>
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    
+    <!-- JS for Back To TOP -->
+    <script>
+        $(document).ready(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 50) {
+                    $('#back-to-top').fadeIn();
+                } else {
+                    $('#back-to-top').fadeOut();
+                }
+            });
+            // scroll body to 0px on click
+            $('#back-to-top').click(function() {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 750);
+                return false;
+            });
+        });
+    </script>
+
 </body>
 
 </html>
-
-<!-- 
-Frozen Food
-Fresh Food
-Bevarges
-Home Health
-Pet Food
-	(Fresh Food)
-		TBone Steak Cheddar Cheese Navel Oranges Bananas Grapes Apples Peaches
-		(Cheddar Cheese)
-			500gram 1000gram
-
-	(Frozen Food)
-		Hamburger Patiee Fish Fingers Shelled Prawns Tub Ice Cream
-		(Fish Fingers)
-			600gram 1000gram
-		(Tub Ice Cream)
-			1litre 2litre
-	(Home Health)
-		Bath Soap Panadol Washing Powder Garbage Bags Laundry Bleach
-			(Panadol)
-				Pack24 Bottle50
-			(Garbage Bags)
-				Small Pack10 Large Pack50
-	(Bevarges)
-		Coffee Earl Grey Tea Bags Chocolate Bar
-			(Coffee)
-				200gram 600gram
-			(Earl Grey Tea Bags)
-				Pack25 Pack100 Pack200
-	(Pet Food)
-		Bird Food Cat Food Dry Dog Food Fish Food
-		(Dry Dog Food)
-			1kg Pack 5kg Pack -->
